bb7e6647f30dac7576c631ad957608c9db612eda
jfchen19 <jchenf19@gmail.com>
2021-02-19 02:48:52 +0800

    CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  before_action :find_task, except: [:index, :create]
+
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+
+  def show
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to '/', notice: '任務建立成功'
+    else
+      render :index, notice: '任務建立失敗'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to '/', notice: '任務修改成功'
+    else
+      render :edit, notice: '任務修改失敗'
+    end
+  end
+
+  def destroy
+    @task.destroy if @task
+    redirect_to '/', notice: '任務刪除成功'
+  end
+
+  private
+  def find_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:title, :subject)
+  end
+end
