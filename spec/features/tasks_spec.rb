e7ce6275f0c6bb476dd65bab7231400a1513bc39
jfchen19 <jchenf19@gmail.com>
2021-02-22 14:31:08 +0800

    add factories/task.rb and update feature/task.rb

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  # let(:title) { Faker::Lorem.sentence }
-  # let(:subject) { Faker::Lorem.paragraphs }
-  before(:all) do
-    @task = Task.create(title: 'test title', subject: 'test subject')
-  end
+  let!(:task) { Task.create(title: Faker::Lorem.sentence, subject: Faker::Lorem.paragraphs)}  # let! 表示在 before 就先做了
 
   scenario "create a new task" do
     visit root_path
-    fill_in "標題", with: "test title2"
-    fill_in "主旨", with: "test subject2"
+    fill_in "標題", with: "test title"
+    fill_in "主旨", with: "test subject"
     click_button "Create Task"
 
     expect(page).to have_content "任務建立成功"
-    expect(page).to have_content "test title2"
-    expect(page).to have_content "test subject2"
+    expect(page).to have_content "test title"
+    expect(page).to have_content "test subject"
+    expect(Task.all.size).to be 2
+  end
+
+  scenario "view a task" do
+    visit task_path(task)
+
+    expect(page).to have_content(task[:title])
+    expect(page).to have_content(task[:subject])
   end
 
   scenario "edit a task" do
@@ -36,5 +40,6 @@ RSpec.feature "Tasks", type: :feature do
 
     expect(page).to have_content "任務刪除成功"
     expect(page).not_to have_content "test title1"
+    expect(Task.all.size).to be 0
   end
 end
