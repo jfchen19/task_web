1111b78616bc292fa9be3dd856afe4d738f56706
jfchen19 <jchenf19@gmail.com>
2021-02-25 17:00:53 +0800

    switch language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  # before_action :switch_locale
  
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+  
+    I18n.locale = session[:locale] || I18n.default_locale
   end
+
+  # def set_locale
+  #   I18n.locale = params[:locale] || I18n.default_locale
+  # end
+
+  # def switch_locale(&action)
+  #   locale = params[:locale] || I18n.default_locale
+  #   I18n.with_locale(locale, &action)
+  # end
+
+  # def default_url_options(options = {})
+  #   { :locale => I18n.locale }.merge options
+  # end
+
+  # def extract_locale_from_tld
+  #   parsed_locale = request.subdomains.first
+  #   I18n.available_locales.map(&:to_s).include?(parsed_locale) ?      
+  #   parsed_locale : nil
+  # end
 end
