abfbaa168ee3eb6fa4d3f73d960f3a485745272f
jfchen19 <jchenf19@gmail.com>
2021-03-08 17:33:59 +0800

    update I18n

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,10 @@ class Task < ApplicationRecord
   scope :with_order, -> (order) { order(created_at: order) if order}
 
   def end_time_after_start_time
+    return if end_time.blank? || start_time.blank?
+
     if end_time < start_time
-      errors.add(:end_time, "不能比開始時間早！")
+      errors.add(:end_time, I18n.t('activerecord.errors.models.task.attributes.end_time_after_start_time'))
     end
   end
 end
