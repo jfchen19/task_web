49af0689d2487b16010068933ad2514d83ab1d0f
jfchen19 <jchenf19@gmail.com>
2021-03-08 16:54:52 +0800

    add start time and end time

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,12 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
-  validates :subject, presence: true
+  validates :title, :subject, :start_time, :end_time, presence: true
+  validate :end_time_after_start_time
 
   scope :with_order, -> (order) { order(created_at: order) if order}
+
+  def end_time_after_start_time
+    if end_time < start_time
+      errors.add(:end_time, "不能比開始時間早！")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_181039) do
+ActiveRecord::Schema.define(version: 2021_03_08_080810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_02_18_181039) do
     t.string "subject"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
 end
