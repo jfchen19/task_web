2ad05a8b07257eda3c6184dd28bd653460eff442
jfchen19 <jchenf19@gmail.com>
2021-03-09 14:09:56 +0800

    update task_spec.rb

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :task do
     title { Faker::Lorem.sentence }
     subject { Faker::Lorem.paragraphs }
+    start_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 3) }
+    end_time { Faker::Time.between(from: DateTime.now + 3, to: DateTime.now + 10) }
   end
 end
