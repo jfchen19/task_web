7163b5b0edd857466c95cc2f487c4eb2d522a47f
jfchen19 <jchenf19@gmail.com>
2021-06-07 04:49:42 +0800

    administrator can edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+
   def index
     @users = User.all.includes(:tasks).page(params[:page]).per(5)
   end
@@ -17,8 +19,23 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_root_path, notice: t('.notice')
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :nickname, :admin)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
